b54cdeda63e2f6916729e2393307d2bedde71f6a
Lovetus <amk1005591@student.hamk.fi>
2024-10-20 16:46:54 +0300

    luotu tekijä

--- tulostus/src/App.java
+++ tulostus/src/App.java
@@ -1,9 +1,15 @@
+import java.util.LongSummaryStatistics;
+
 public class App {
     public static void main(String[] args) throws Exception {
       
         System.out.println("Hei olen Tulostin-ohjelma");
         System.out.println("ohjelman tekijä:");
 
-        
+        String tekija;
+        tekija = "Lotta";
+
+System.out.println("Ohjelman tekija: " + tekija);
+
     }
 }
